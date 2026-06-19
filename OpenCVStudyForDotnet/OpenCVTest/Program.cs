5c5038a93132985f0b0c5611308d947a31db4199
hundong2 <ehdgns104@gmail.com>
2026-06-19 13:43:57 +0900

    Update initialize for Opencv dotnet

---
+++ OpenCVStudyForDotnet/OpenCVTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using OpenCvSharp;
+
+namespace OpenCVTestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cv2.GetVersionString());
+        }
+    }
+}
